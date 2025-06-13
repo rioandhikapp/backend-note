ac942ca0682e5c114dec18ddc13f88354153c915
RioAPP010 <rioandhikaa702@gmail.com>
2025-06-13 17:50:51 +0700

    a

--- backend-note/public/index.php
+++ backend-note/public/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 session_start();
 require_once 'src/db.php';
 
